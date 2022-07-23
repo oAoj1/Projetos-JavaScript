bfba8cae69103ac150993c3f05d434a43b715742
oAoj1 <joaogbmarques@gmail.com>
2022-07-22 23:07:19 -0300

    calculadora, versao final

---
+++ calculadora/calculadora.js
@@ -0,0 +1,22 @@
+var resultado = document.querySelector('#resultado')
+
+function adicionar(num){
+    var res = document.querySelector('#resultado').innerHTML
+    resultado.innerHTML = res + num
+}
+
+function limpar(){
+    resultado.innerHTML = ''
+}
+
+function voltar(){
+    var resultado = document.querySelector('#resultado').innerHTML
+    document.querySelector('#resultado').innerHTML = resultado.substring(0,resultado.length - 1)
+}
+
+function calcular(){
+    var resultado = document.querySelector('#resultado').innerHTML
+    if(resultado){
+        document.querySelector('#resultado').innerHTML = eval(resultado)
+    }
+}
