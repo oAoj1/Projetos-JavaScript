d9eb928627c9ae0c02d86f124f839af915d49bca
oAoj1 <joaogbmarques@gmail.com>
2022-07-23 13:27:08 -0300

    coloca formulario, atualiza adinha, tira carrossel

---
+++ adivinhe/advinha.js
@@ -0,0 +1,8 @@
+var c = document.querySelector('#escolha1')
+var t = document.querySelector('#escolha2')
+var intro = document.querySelector('div.container-colum-intro')
+
+function esconder(num){
+    intro.style.display = 'none'
+    let cinco = esconder()
+}
