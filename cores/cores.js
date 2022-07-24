e4ef12e8ff55e199862431e47139c29c9d078cbe
oAoj1 <joaogbmarques@gmail.com>
2022-07-23 22:06:14 -0300

    cores update

--- cores/cores.js
+++ cores/cores.js
@@ -1,19 +1,4 @@
-var fundo = window.document.getElementById('fundo')
-
-function vermelho(){
-    fundo.style.backgroundColor = 'red'
-    fundo.style.color = 'white'
-    fundo.style.transition = '0.3s'
-}
-
-function verde(){
-    fundo.style.backgroundColor = 'green'
-    fundo.style.color = 'white'
-    fundo.style.transition = '0.3s'
-}
-
-function azul(){
-    fundo.style.backgroundColor = 'blue'
-    fundo.style.color = 'white'
-    fundo.style.transition = '0.3s'
+function mudar(cor){
+    var fundo = window.document.getElementById('fundo')
+    fundo.style.backgroundColor = cor
 }
