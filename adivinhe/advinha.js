cee44d441d6a391d7446a310ee6b68c52d55e435
oAoj1 <joaogbmarques@gmail.com>
2022-07-23 21:33:16 -0300

    adivinhe update

--- adivinhe/advinha.js
+++ adivinhe/advinha.js
@@ -1,8 +1,28 @@
-var c = document.querySelector('#escolha1')
-var t = document.querySelector('#escolha2')
-var intro = document.querySelector('div.container-colum-intro')
+function enviar(){
+    var n = document.querySelector('#numtext')
+    var resultado = document.querySelector('#resultado')
 
-function esconder(num){
-    intro.style.display = 'none'
-    let cinco = esconder()
+    if(n.value.length == 0){
+        alert('Coloque um valor valido')
+    }else if(n.value < 0 || n.value > 5){
+        alert(`${n.value} invalido, apenas entre 0 e 5`)
+    }else{
+        var numero = n.value
+        var pc = Math.round(Math.random()*5)
+        
+        var c = 1
+        var maximo = 5
+        while(c <= maximo){
+            console.log(pc)
+            if(numero == pc){
+                resultado.innerHTML = 'Voce acertou'
+                break
+            }else{
+                resultado.innerHTML = 'Voce errou'
+            }
+            n.value = ''
+            n.focus()
+            c++
+        }
+    }
 }
