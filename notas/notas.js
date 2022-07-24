d4b46de971495bee3b32f62eb217a2166f20a9da
oAoj1 <joaogbmarques@gmail.com>
2022-07-24 00:56:59 -0300

    media de nota

---
+++ notas/notas.js
@@ -0,0 +1,10 @@
+function enviar(){
+    var a = document.querySelector('#txtaluno')
+    var m = document.querySelector('#txtmateria')
+    var n = document.querySelector('#txtnotas')
+    var resultado = document.querySelector('#resultado')
+    var imagem = document.querySelector('#imagem')
+
+    
+
+}
