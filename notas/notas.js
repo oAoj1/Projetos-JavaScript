7d28bb84ad0adb99b1029903cb2bc217f0afb300
oAoj1 <joaogbmarques@gmail.com>
2022-07-25 10:50:08 -0300

    notas final version 1.0

--- notas/notas.js
+++ notas/notas.js
@@ -1,15 +1,14 @@
 let a = document.querySelector('#txtaluno')
-let m = document.querySelector('#txtmateria')
+let d = document.getElementsByName('disciplina')
 let n = document.querySelector('#txtnota')
+let img = document.querySelector('#imagem')
 
 let lista_nota = []
 let total_notas = 5
 
 let tabela = document.querySelector('#tabela')
 let res_nota = document.querySelector('#resultado-notas')
-let resultado = document.querySelector('#resultado')
-
-
+let resultado = document.querySelector('div#resultado')
 
 function adicionar(){
     if(n.value.length == 0){
@@ -22,12 +21,12 @@ function adicionar(){
         n.focus()
     }else{
         if(lista_nota.length < total_notas){
-            lista_nota.push(n.value)
+            lista_nota.push(Number(n.value))
             let escrito = document.createElement('option')
             escrito.text = `Nota: ${n.value}`
             tabela.appendChild(escrito)
             res_nota.innerHTML = ''
-
+            
             console.log(lista_nota)
             n.value = ''
             n.focus()
@@ -35,16 +34,92 @@ function adicionar(){
             alert('Maximo de notas atingido')
             n.value = ''
         }
-        let soma = 0
-        let media = 0
-
-        for(let cont in lista_nota){
-            soma += lista_nota
-        }
-        console.log()
     }
 }
 
 function enviar(){
+    let soma = 0
+    let media = 0
 
+    for(let pos in lista_nota){
+        soma += lista_nota[pos]
+    }
+    media = soma / total_notas
+
+    if(a.value.length == 0 || lista_nota.length < total_notas){
+        alert('Dados insuficientes')
+    }else{
+        if(d[0].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Mátematica<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/matematica.png'
+        }
+        if(d[1].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Poruguês<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/portugues.png'
+        }
+        if(d[2].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Tecnologia<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/tecnologia.png'
+        }
+        if(d[3].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Educação Física<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/ed fisica.png'
+        }
+        if(d[4].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Artes<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/artes.png'
+        }
+        if(d[5].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Física<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/fisica.png'
+        }
+        if(d[6].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> História<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/historia.png'
+        }
+        if(d[7].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Química<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/quimica.png'
+        }
+        if(d[8].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Biologia<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/biologia.png'
+        }
+        if(d[9].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Inglês<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/ingles.png'
+        }
+        if(d[10].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Sociologia<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/sociologia.png'
+        }
+        if(d[11].checked){
+            resultado.innerHTML += `<strong>Nome do aluno:</strong> ${a.value}<br>`
+            resultado.innerHTML += `<strong>Disciplina:</strong> Filosofia<br>`
+            resultado.innerHTML += `<strong>Média final:</strong> ${media}<br>`  
+            img.src = 'imagens/filosofia.png'
+        }
+    }
 }
